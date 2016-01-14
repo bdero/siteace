a8358ae404b4a94d92e2e8c70231cac1de780438
Brandon DeRosier <x@bdero.me>
2016-01-13 21:02:21 -0500

    Only allow inserting when the user is logged in

--- shared/collections.js
+++ shared/collections.js
@@ -1,1 +1,14 @@
 Websites = new Mongo.Collection("websites");
+
+Websites.allow({
+  insert: function(userId, doc) {
+    if (userId) {
+      doc.submittedBy = Meteor.user()._id;
+      doc.createdOn = new Date();
+
+      return doc.title && doc.url && doc.description;
+    }
+
+    return false;
+  }
+});
