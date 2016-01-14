4b585ec08cd1876b337e6b7e8586688d51245666
Brandon DeRosier <x@bdero.me>
2016-01-13 21:42:51 -0500

    Sort images by votes descending

--- client/client.js
+++ client/client.js
@@ -5,7 +5,7 @@
 // helper function that returns all available websites
 Template.website_list.helpers({
   websites:function(){
-    return Websites.find({});
+    return Websites.find({}, {sort: {rating: -1}});
   }
 });
 
