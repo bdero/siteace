9630d72442410c5d9fb2b9a1fcbc953ebef836f7
Brandon DeRosier <x@bdero.me>
2016-01-13 23:37:53 -0500

    Added comment submission check

--- shared/collections.js
+++ shared/collections.js
@@ -3,11 +3,26 @@ Websites = new Mongo.Collection("websites");
 Websites.allow({
   insert: function(userId, doc) {
     if (userId) {
-      doc.submittedBy = Meteor.user()._id;
+      doc.submittedBy = userId;
       doc.createdOn = new Date();
       doc.upvotes = doc.downvotes = 0;
 
       return doc.title && doc.url && doc.description;
+    }
+
+    return false;
+  }
+});
+
+Comments = new Mongo.Collection("comments");
+
+Comments.allow({
+  insert: function(userId, doc) {
+    if (userId) {
+      doc.submittedBy = userId;
+      doc.createdOn = new Date();
+
+      return doc.message && Websites.find({_id: doc.website_id}).count();
     }
 
     return false;
